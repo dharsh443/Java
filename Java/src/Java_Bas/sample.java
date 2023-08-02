d075393e50d021b0104bc3daca07fbe6d422160d
Hp <Hp@DESKTOP-7U46JFR>
2023-08-02 14:58:37 +0530

    Uploading Java projects to Git and GitHub

---
+++ Java/src/Java_Bas/sample.java
@@ -0,0 +1,13 @@
+package Java_Bas;
+
+public class sample {
+  
+	public static void main(String args[])   
+	{  
+	int n1 = 31, n2 = 22, sum;  
+	sum = n1 + n2;  
+	System.out.println("The sum of numbers is: "+sum);  
+	}  
+	}  
+
+
